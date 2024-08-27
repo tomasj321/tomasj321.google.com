458607f3eb65c9e54200930f31231f8d1b24882c
tomasj321 <junco.t.a.06@gmail.com>
2024-08-27 13:57:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showEvent(eventId) {
+    document.getElementById(eventId).style.display = 'block';
+}
+
+function closeEvent(eventId) {
+    document.getElementById(eventId).style.display = 'none';
+}
